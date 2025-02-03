85623b1b6d64cdcf59f4132d056d32db53632006
brentjolicoeur <brentjolicoeur@gmail.com>
2025-02-03 15:22:01 -0500

    added player to screen

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-import pygame
+import pygame # type: ignore
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     playing = True
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while playing:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
