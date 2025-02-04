b311c829c28a3291b0fef86b6e137841e0ebe856
brentjolicoeur <brentjolicoeur@gmail.com>
2025-02-03 23:42:06 -0500

    added asteriods, shots, and collision detection

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
 import pygame # type: ignore
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -9,16 +13,46 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     playing = True
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    asteroid_field = AsteroidField()
+
 
     while playing:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.check_collisions(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.check_collisions(shot):
+                    shot.kill()
+                    asteroid.kill()
+                
+        
         screen.fill("black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
