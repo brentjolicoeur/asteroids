88cc674482def549a8aed9b4fb6f50f9038891af
brentjolicoeur <brentjolicoeur@gmail.com>
2025-06-17 11:10:18 -0400

    player vs asteroid collision detection

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-import pygame
+import pygame, sys
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from asteroid import Asteroid
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers =(updatable,)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.detect_collision(player):
+                print("Game over!")
+                sys.exit()
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     
